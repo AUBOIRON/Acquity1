5702471f6434193c88f027a826e853540733b8dd
AUBOIRON <auboiron_sacha@yahoo.fr>
2019-02-07 17:11:19 +0100

    new

--- bootstrap_ext/js/acquity.js
+++ bootstrap_ext/js/acquity.js
@@ -1,36 +1,3 @@
-function marquee(idWrapper,idMarquee,vitesse)
- 
-{
-    var oIdWrapper=$('#'+idWrapper);
-    var oIdMarquee=$('#'+idMarquee);
- 
-    var width=oIdMarquee.width()/2;
-    var width2=oIdWrapper.width()/2;
- 
-    id_inst=setTimeout(function() {marquee(idWrapper,idMarquee,vitesse)},vitesse);
-  
-    var l=parseInt(oIdMarquee.css('left'));
-    oIdMarquee.css({left:(l-1)+'px'});
- 
-    if((-parseInt(oIdMarquee.css('left')))>=(width))
-    {
-        oIdMarquee.css({left:(width2)+'px'});
-    }
-}
- 
-$(document).ready(
-function(){
-    marquee('marquee-wrapper','marquee',2); //changer le chiffre 10 pour modifier la vitesse <img src="../../bundles/tinymce/vendor/tiny_mce/plugins/emotions/img/clin.png" title=";)" alt=";)">
-}
-);
-$(document).ready(function(){
-     $("#marquee-wrapper").hover(function(){
-        clearTimeout(id_inst);
-    }, function(){
-        marquee("marquee-wrapper","marquee",2);
-    });
-});
-
 (function($) {
     "use strict"; // Start of use strict
   
@@ -67,7 +34,90 @@ $(document).ready(function(){
     })
   
   })(jQuery); // End of use strict
-
+  
+  function defilImgHrz(el,srcs,pas,tps) {
+    if(typeof el=="string") { el = document.getElementById(el); }
+    var tps = tps || 50;
+    var pas = pas || 1;
+    var imgs = [];
+    var offset = 0;
+    for(var i=0,l=srcs.length;i<l;i++) {
+      var img = new Image();
+      img.src = srcs[i];
+      imgs.push(img);
+      img.style.height=el.offsetHeight+"px";
+      img.style.position = "absolute";
+      img.style.left = offset+"px";
+      el.appendChild(img);
+      offset += img.offsetWidth;
+    }
+    var first = 0;
+    var last = imgs.length-1;
+   
+    (function d() {
+      for(var i=0,l=imgs.length;i<l;i++) {
+        var left = parseInt(imgs[i].style.left,10);
+        imgs[i].style.left = (left-pas)+"px";
+        if(i==first && (left-pas+imgs[i].offsetWidth)<0) {
+          imgs[i].style.left = 
+          (parseInt(imgs[last].style.left,10)+imgs[last].offsetWidth-(i==0?pas:0))+"px";
+          last = first++;
+          if(first>imgs.length-1) { first = 0; }
+        }
+      }
+      setTimeout(d,tps);
+    })();
+  }
+   
+  // Fonction de défilement verticale
+  function defilImgVrt(el,srcs,pas,tps) {
+    if(typeof el=="string") { el = document.getElementById(el); }
+    var tps = tps || 50;
+    var pas = pas || 1;
+    var imgs = [];
+    var offset = 0;
+    for(var i=0,l=srcs.length;i<l;i++) {
+      var img = new Image();
+      img.src = srcs[i];
+      imgs.push(img);
+      img.style.width=el.offsetWidth+"px";
+      img.style.position = "absolute";
+      img.style.top = offset+"px";
+      el.appendChild(img);
+      offset += img.offsetHeight;
+    }
+    var first = 0;
+    var last = imgs.length-1;
+   
+    (function d() {
+      for(var i=0,l=imgs.length;i<l;i++) {
+        var top = parseInt(imgs[i].style.top,10);
+        imgs[i].style.top = (top-pas)+"px";
+        if(i==first && (top-pas+imgs[i].offsetHeight)<0) {
+          imgs[i].style.top = (parseInt(imgs[last].style.top,10)+imgs[last].offsetHeight-(i==0?pas:0))+"px";
+          last = first++;
+          if(first>imgs.length-1) { first = 0; }
+        }
+      }
+      setTimeout(d,tps);
+    })();
+  }
+  // Appel des deux fonctions de défilement au chargement de ma page .
+  window.onload=function() {
+    defilImgVrt('bandeau_vertical',[
+    "images/Solocal.png",
+      "images/ensam.png",
+      "images/CH LEZIGNAN.png",
+    "images/Bein_sport.png"
+     
+    ]);
+    defilImgHrz('bandeau_Horizontal',[
+      "images/Solocal.png",
+      "images/ensam.png",
+      "images/CH LEZIGNAN.png",
+    "images/Bein_sport.png"
+    ]);
+  };
 
  
 

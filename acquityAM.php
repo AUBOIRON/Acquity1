a7684d08302220d9df83c3b0b6ac3bdd758780df
AUBOIRON <auboiron_sacha@yahoo.fr>
2019-02-07 10:17:42 +0100

    5

--- acquityAM.php
+++ acquityAM.php
@@ -31,14 +31,6 @@
 	<link href="bootstrap_ext/css/acquity.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	
-<script type="text/javascript">
-function ouvrirLien ()
-         {
-         var link = "https://halshs.archives-ouvertes.fr/file/index/docid/490675/filename/management_public.pdf";
-         window.open(lien);
-         }
-</script>
 </head>
 <body>
 
